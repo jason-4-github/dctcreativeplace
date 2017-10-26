a4e0d23448f7565a165977060d41decce28c0b00
Jason Hsu <jason80d@gmail.com>
2017-10-26 14:32:40 +0800

    feat: setting the redux constructure and ant config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import configureStore from './store/configureStore';
+import Page1 from './containers/Type1Container';
+import Page2 from './containers/Type2Container';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'antd/dist/antd.less';
+import './styles/index.less';
+
+const store = configureStore();
+
+const routerSet = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Page1} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    {routerSet()}
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
